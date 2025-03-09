dd629b26120509a9e10a192f1c388cf755da9473
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2025-03-10 00:10:28 +0530

    add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"})) //It will accept only 16kb size limit of json file.
-app.use(express.urlencoded())//This is okay but if u want to deep dive and break this into objects then this is the process below.
+// app.use(express.urlencoded())//This is okay but if u want to deep dive and break this into objects then this is the process below.
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 
 app.use(express.static("public")) //This is a public folder to store public data like pdfs, imagaes etc.
@@ -26,7 +26,7 @@ import userRouter from './routes/user.routes.js'
 app.use("/users",userRouter)
 
 //If we are defining our api then we can write routes for register like this: 
-app.use("/api/v1/users",userRouter) //URL: https://localhost:8000/api/v1/users/register
+app.use("/api/v1/users",userRouter) //URL: http://localhost:8000/api/v1/users/register
 //Here v1: verson of api
 
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,84 @@
 import { response } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.models.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
+const registerUser = asyncHandler(async (req, res) => {
+    /*Steps for registering a user:) 
+    - Get user details from frontend(e.g using react or postman),
+    - Validation(e.g check empty fields),
+    - Check if user alraedy exists:  [check using username, email]
+    - Check for images, check for avatar
+    - Upload them to cloudinary, 
+    - Also check in cloudinary that avatar is received or not,
+    - Create user object - create entry in DB,
+    - Remove password and refresh token field from response,
+    - Check for user creation
+    - Return res
+    */
+    const { fullName, email, username, password } = req.body
+    console.log("email: ", email)
 
-const registerUser = asyncHandler(async (res, req) => {
-    res.status(200).json({
-        message: "Okay!!"
+    // if(fullName==="")
+    // {
+    //     throw new ApiError(400, "Full name is required ")
+    // } // Manual check all the fields and it will be lengthy..
+
+    //Another method will be this below: 
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+    //Check for existing user!!
+    const existingUser = User.findOne({ //Here we can also use User.find()
+        $or: [{ username }, { email }]
     })
+    if (existingUser) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+
+    if (avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    //Upload On cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    //Check this avatar is present or not in the db else it may cause crash in the db
+    if (!avatar) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password,
+        username:username.toLowerCase()
+    })
+
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken" //This fields are not required that's why '-' symbol is used there
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user!")
+    }
+
+    return response.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully!")
+    )
+
 })
-
-
 
 export { registerUser }
